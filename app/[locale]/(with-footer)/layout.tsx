a0c724e61ef1f5fb2f30f2b3e62ee621f61e8066
wms-why <167847970+wms-why@users.noreply.github.com>
2024-06-29 22:38:03 +0800

    添加google统计代码

--- app/[locale]/(with-footer)/layout.tsx
+++ app/[locale]/(with-footer)/layout.tsx
@@ -1,10 +1,21 @@
 import Footer from '@/components/home/Footer';
+import Script from 'next/script';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <main className='mx-auto flex w-full flex-1'>{children}</main>
       <Footer />
+
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-EVP5RGLCG4" strategy="afterInteractive"></Script>
+      <Script strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-EVP5RGLCG4');
+        `}
+      </Script>
     </>
   );
 }
